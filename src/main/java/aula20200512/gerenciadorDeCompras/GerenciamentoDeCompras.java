0ca3f027254f0f0e3b66eb6682b5ecca5ccb6999
vitor-barbosa <viitorrsbarbosa@gmail.com>
2020-05-12 23:25:43 -0300

    * utilizando JUnit * Inicio da resposta do conteudo de testes

---
+++ src/main/java/aula20200512/gerenciadorDeCompras/GerenciamentoDeCompras.java
@@ -0,0 +1,12 @@
+package aula20200512.gerenciadorDeCompras;
+
+public class GerenciamentoDeCompras {
+    void efetuarCompra(String cpfCliente, Double valor) {
+
+    }
+    void renovarLimiteDeCredito(String cpfCliente) {
+    }
+    double getSaldoLimiteDeCredito(String cpfCliente) {
+        cpfCliente - 
+    }
+}
