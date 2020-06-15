6bf0e3538af3c4d5ace8c598c1f5895e11629496
vitor.barbosa <vitor.barbosa@accountfy.com>
2020-06-15 20:52:54 -0300

    Remoção de interface desnecessária

--- src/main/java/atividadeForumSegundoBimestre/access/Livro.java
+++
@@ -1,9 +0,0 @@
-package atividadeForumSegundoBimestre.access;
-
-public interface Livro {
-    String getDataDeLancamento();
-    String getAutor();
-    String getTitulo();
-    Integer getNumeroDePaginas();
-    String getGenero();
-}
