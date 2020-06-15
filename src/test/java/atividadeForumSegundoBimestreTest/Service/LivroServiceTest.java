7a197c26247f84cd4dfbb1337e7ca8470ff84938
vitor.barbosa <vitor.barbosa@accountfy.com>
2020-06-15 20:01:12 -0300

    criação das classes de testes

---
+++ src/test/java/atividadeForumSegundoBimestreTest/Service/LivroServiceTest.java
@@ -0,0 +1,39 @@
+package atividadeForumSegundoBimestreTest.Service;
+
+import atividadeForumSegundoBimestreTest.entity.Biblioteca;
+import atividadeForumSegundoBimestreTest.entity.Livro;
+import atividadeForumSegundoBimestreTest.repository.BibliotecaRepository;
+import atividadeForumSegundoBimestreTest.repository.LivroRepository;
+import atividadeForumSegundoBimestreTest.service.LivroService;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+public class LivroServiceTest {
+
+    @Test
+    void alterarTituloDoLivroTest() throws Throwable {
+        ArgumentCaptor<Livro> livroArgumentCaptor = ArgumentCaptor.forClass(Livro.class);
+        ArgumentCaptor<Biblioteca> bibliotecaArgumentCaptor = ArgumentCaptor.forClass(Biblioteca.class);
+        LivroRepository livroRepository = Mockito.mock(LivroRepository.class);
+        BibliotecaRepository bibliotecaRepository = Mockito.mock(BibliotecaRepository.class);
+        LivroService livroService = new LivroService(livroRepository);
+        BibliotecaService bibliotecaService = new BibliotecaService(bibliotecaRepository);
+
+        when(livroService.findById("1"))
+                .thenReturn(
+                        new Livro(
+                                "O Senhor dos Anéis: A Sociedade do Anel",
+                                "J. R. R. Tolkien",
+                                423,
+                                "1954",
+                                "Literatura fantástica"));
+
+        bibliotecaService.reservarLivroPorTitulo("O Senhor dos Anéis: A Sociedade do Anel");
+
+        assertThat(livroRepository.findById("1")).contains("A Sociedade do Anel");
+    }
+}
