f7929f231129adafa40a01c782f4db22977fd3e2
vitor.barbosa <vitor.barbosa@accountfy.com>
2020-06-15 19:43:19 -0300

    inclusão de interface do livro

---
+++ src/main/java/atividadeForumSegundoBimestreTest/access/Livro.java
@@ -0,0 +1,9 @@
+package atividadeForumSegundoBimestreTest.access;
+
+public interface Livro {
+    String getDataDeLancamento();
+    String getAutor();
+    String getTitulo();
+    Integer getNumeroDePaginas();
+    String getGenero();
+}
